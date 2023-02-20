b5460472286325e4b9c9b8aea40bfa90241af767
albetnov <ryxinfrvr@gmail.com>
2023-02-20 14:39:17 +0700

    feat: init fiber and viper

---
+++ server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+)
+
+/**
+* Load configuration
+ */
+func init() {
+	// load from file
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed, restarting:", e.Name)
+	})
+	viper.WatchConfig()
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Errorf("fatal error regrading config file: %w", err))
+	}
+
+	// set default
+	viper.SetDefault("PORT", 3000)
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		c.SendStatus(200)
+
+		return c.JSON(fiber.Map{
+			"message": "Welcome to Rent-N-Go Backend Entrypoint",
+			"status":  200,
+		})
+	})
+
+	app.Listen(fmt.Sprintf(":%d", viper.GetInt("PORT")))
+}
